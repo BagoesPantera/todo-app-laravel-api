4b8edd69f94866d7e0cf998423529ab778801d58
BagoesPantera <bagoespantera01@gmail.com>
2023-07-27 14:19:04 +0800

    init laravel 10

--- routes/api.php
+++ routes/api.php
@@ -9,38 +9,11 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
 |
 */
-
-$router->get('/', function(){
-    return response()->json(['message' => 'Connected to server'],200);
-});
-
-Route::group([
-    'namespace' => 'App\Http\Controllers',
-    'prefix' => 'auth'
-],  function(){
-        Route::post('register', 'AuthController@register');
-        Route::post('login', 'AuthController@login');
-    }
-);
-
-Route::group([
-    'namespace' => 'App\Http\Controllers',
-    'prefix' => 'todo',
-    'middleware' => ['auth:sanctum']
-], function () {
-        Route::get('/', 'TodoController@index');
-        Route::get('/{id}', 'TodoController@show');
-        Route::post('add', 'TodoController@add');
-        Route::post('update/{id}', 'TodoController@update');
-        Route::delete('delete/{id}', 'TodoController@delete');
-    }
-);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
