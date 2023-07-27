31c45211f6735aea48811f93de77956370ff9dd6
BagoesPantera <bagoespantera01@gmail.com>
2023-07-27 15:06:46 +0800

    update and delete todo

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -53,4 +53,56 @@ class TodoController extends Controller
             return response()->json(['message' => 'Data failed to add.'], 500);
         }
     }
+
+    //
+    public function update($id, Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string',
+            'description' => 'required|string',
+        ]);
+
+        $todo = Todo::find($id);
+
+        if(!$todo){
+            return response()->json(['message' => 'Data not found',], 404);
+        }
+
+        $todo->title = $request->title;
+        $todo->description = $request->description;
+
+        if ($todo->userId == Auth::id()) {
+            $save = $todo->save();
+
+            if ($save) {
+                return response()->json(['message' => 'Data updated successfully.'], 200);
+            } else {
+                return response()->json(['message' => 'Data failed to update.'], 500);
+            }
+        } else {
+            return response()->json(['message' => 'You are not allowed to update this data.'], 403);
+        }
+    }
+
+    //
+    public function delete($id)
+    {
+        $todo = Todo::find($id);
+
+        if(!$todo){
+            return response()->json(['message' => 'Data not found',], 404);
+        }
+
+        if($todo->userId == Auth::id()){
+            $delete = $todo->delete();
+
+            if ($delete) {
+                return response()->json(['message' => 'Data deleted successfully.'], 200);
+            } else {
+                return response()->json(['message' => 'Data failed to delete.'], 500);
+            }
+        } else {
+            return response()->json(['message' => 'You are not allowed to delete this data.'], 403);
+        }
+    }
 }
